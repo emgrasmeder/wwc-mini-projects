2666191b38192e3d4809442f1479bad2af29af02
Emma <emma@gras.org>
2015-08-17 19:04:53 -0400

    add template file for people to edit

---
+++ fibonacci/fibonacci.py
@@ -0,0 +1,30 @@
+# Edit this file in your text editor or IDE and run it from the command line
+
+def fib(x):
+    if x = 0:
+        return "Error: Fibonacci sequence is indexed starting with 1"
+    elif x == 1:
+        return 0
+    elif x == 2:
+        return 1
+    elif x == 3:
+        return 1
+    elif x == 4:
+        return 2
+    elif x == 5:
+        return 3
+    elif x == 6:
+        return 5
+    elif x == 7:
+        return 8
+
+
+if __name__ == "__main__":
+    assert fib(1) == 0
+    assert fib(2) == 1
+    assert fib(3) == 1
+    assert fib(4) == 2
+    assert fib(5) == 3
+    assert fib(6) == 5
+    assert fib(7) == 8
+    assert fib(83) == 99194853094755497
